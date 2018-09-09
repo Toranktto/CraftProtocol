f45feafb43087b404d55c37176f2eee90823c916
Toranktto <toranktto@gmail.com>
2018-09-09 19:09:22 +0200

    Added world type enum

---
+++ CraftProtocol/World/WorldType.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+
+class WorldType(object):
+    DEFAULT = "default"
+    FLAT = "flat"
+    LARGE_BIOMES = "largeBiomes"
+    AMPLIFIED = "amplified"
+    DEFAULT_1_1 = "default_1_1"
--- CraftProtocol/World/__init__.py
+++ CraftProtocol/World/__init__.py
@@ -3,9 +3,11 @@
 __all__ = [
     "Location",
     "WorldDimension",
-    "WorldDifficulty"
+    "WorldDifficulty",
+    "WorldType"
 ]
 
 from CraftProtocol.World.Location import Location
 from CraftProtocol.World.WorldDimension import WorldDimension
 from CraftProtocol.World.WorldDifficulty import WorldDifficulty
+from CraftProtocol.World.WorldType import WorldType
